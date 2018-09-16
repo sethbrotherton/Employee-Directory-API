0b9e35a219c477eb5ca7eedab50cdae4075e6116
sethbrotherton <sethbrotherton@yahoo.com>
2018-09-16 05:45:24 -0400

    Prev and Next modal buttons work

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ let person;
 let employeeCard;
 let employees;
 $.ajax({
-  url: 'https://randomuser.me/api/?results=12&nat=us',
+  url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',
   success: function(data) {
     employees = data.results;
     appendCardsToPage();
-    addModal();
+    addModal(num);
   }
 });
 
@@ -24,15 +24,19 @@ function cardModal () {
 
 
 let num;
+let arrayNum;
 function addModal(num) {
-$cards.click(function() {
-  //num = 0;
-  num = parseInt(($(this).attr('id')));
-  $('body').append(
-    `<div class="modal-container" id="${num + 12}">
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
+    $cards = $('.card');
+    $cards.click(function(event) {
+    num = 0;
+    num += parseInt(($(this).attr('id')));
+    //arrayNum = arrayOfCards.indexOf(event.target.attr('id'));
+    arrayNum = parseInt($(this).attr('id'));
+    $('body').append(
+      `<div class="modal-container" id="${num + 12}">
+          <div class="modal">
+              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+              <div class="modal-info-container">
                 <img class="modal-img" src="${employees[num].picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">${employees[num].name.first} ${employees[num].name.last}</h3>
                 <p class="modal-text">${employees[num].email}</p>
@@ -51,15 +55,13 @@ $cards.click(function() {
       );
       addCloseModalEvent();
       addNextButtonEvent();
+      addPrevButtonEvent();
   });
-  // addCloseModalEvent();
-  // addNextButtonEvent();
-  // addModal(num);
 }
 
 function addCloseModalEvent() {
   let $modalContainer = $('.modal-container');
-  const $modalCloseButton = $('#modal-close-btn');
+  let $modalCloseButton = $('#modal-close-btn');
   $modalCloseButton.click(function () {
     $modalContainer.remove();
   });
@@ -70,33 +72,74 @@ function addNextButtonEvent() {
   let $nextButton = $('#modal-next');
   $nextButton.click(function() {
     console.log('you did click it');
-    num += 1;
-  //  currentModalId = parseInt(($(this).parentNode.parentNode.attr('id')));
+  //  let arrayNum = 0;
+    arrayNum += 1;
+    let $modalContainer = $('.modal-container');
+    $modalContainer.remove();
     $('body').append(
       `<div class="modal-container">
           <div class="modal">
               <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
               <div class="modal-info-container">
-                  <img class="modal-img" src="${employees[num].picture.large}" alt="profile picture">
-                  <h3 id="name" class="modal-name cap">${employees[num].name.first} ${employees[num].name.last}</h3>
-                  <p class="modal-text">${employees[num].email}</p>
-                  <p class="modal-text cap">${employees[num].location.city}</p>
+                <img class="modal-img" src="${employees[arrayNum].picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${employees[arrayNum].name.first} ${employees[arrayNum].name.last}</h3>
+                <p class="modal-text">${employees[arrayNum].email}</p>
+                <p class="modal-text cap">${employees[arrayNum].location.city}</p>
+                <hr>
+                <p class="modal-text">${employees[arrayNum].phone}</p>
+                <p class="modal-text">${employees[arrayNum].location.street}, ${employees[arrayNum].location.city}, ${employees[arrayNum].location.state} ${employees[arrayNum].location.postcode}</p>
+                <p class="modal-text">Birthday: ${employees[arrayNum].dob.date}</p>
+              </div>
+            </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+          </div>`
+       );
+        addCloseModalEvent();
+        addNextButtonEvent();
+        addPrevButtonEvent();
+    });
+  }
+
+
+  function addPrevButtonEvent() {
+    let $prevButton = $('#modal-prev');
+    $prevButton.click(function() {
+      console.log('you did click it');
+    //  let arrayNum = 0;
+      arrayNum -= 1;
+      let $modalContainer = $('.modal-container');
+      $modalContainer.remove();
+      $('body').append(
+        `<div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                  <img class="modal-img" src="${employees[arrayNum].picture.large}" alt="profile picture">
+                  <h3 id="name" class="modal-name cap">${employees[arrayNum].name.first} ${employees[arrayNum].name.last}</h3>
+                  <p class="modal-text">${employees[arrayNum].email}</p>
+                  <p class="modal-text cap">${employees[arrayNum].location.city}</p>
                   <hr>
-                  <p class="modal-text">${employees[num].phone}</p>
-                  <p class="modal-text">${employees[num].location.street}, ${employees[num].location.city}, ${employees[num].location.state} ${employees[num].location.postcode}</p>
-                  <p class="modal-text">Birthday: ${employees[num].dob.date}</p>
+                  <p class="modal-text">${employees[arrayNum].phone}</p>
+                  <p class="modal-text">${employees[arrayNum].location.street}, ${employees[arrayNum].location.city}, ${employees[arrayNum].location.state} ${employees[arrayNum].location.postcode}</p>
+                  <p class="modal-text">Birthday: ${employees[arrayNum].dob.date}</p>
+                </div>
               </div>
               <div class="modal-btn-container">
                   <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
                   <button type="button" id="modal-next" class="modal-next btn">Next</button>
               </div>
-          </div>`
-       );
-        addCloseModalEvent();
-        addNextButtonEvent();
-    });
-  }
+            </div>`
+         );
+          addCloseModalEvent();
+          addNextButtonEvent();
+          addPrevButtonEvent();
+      });
+    }
 
+let arrayOfCards = [];
 function appendCardsToPage () {
   $.each(employees, function(i, person) {
     $gallery.append(`<div class="card" id="${i}">
@@ -109,6 +152,6 @@ function appendCardsToPage () {
             <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
         </div>
     </div>`);
-    cardModal();
   });
+  arrayOfCards.push(employees);
 }
