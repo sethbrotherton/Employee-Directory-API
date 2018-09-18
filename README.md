d8cc40c63b1966b87a47db5c87b3c2845ab79933
sethbrotherton <sethbrotherton@yahoo.com>
2018-09-18 00:25:36 -0400

    Added comments, minor style changes, and wrote readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Employee-Directory-API
+This project uses jquery and ajax to retrieve info from a public api(a random profile api)
+and formats and displays the info as an employee directory.
+When the page is loaded or refreshed 12 random profiles of individuals from us, au, and gb
+are loaded.  When a profile card is clicked on, more info about that individual is displayed
+in the modal window.  From there, you can navigate to the previous or next profile via the
+previous or next buttons, or you can close the modal window.
+There is a search input dynamically added to the page in the header section.  A keyup
+event is listening to match the search input with the names of the individuals displayed on
+the page, so it will automatically filter out non-matches.
+Changes that I've made to the styling include: bgcolor, font-size of heading,
+transorm(scale) of cards on hover, border-radius of cards, and border of profile image
+in modal view.
--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-
+// Variables selected of use for Ajax call
 const $gallery = $('#gallery');
 let person;
-let employeeCard;
 let employees;
+// Ajax call of random profile generator
+// 12 results of profiles of individuals from GB, US, and AU
 $.ajax({
-  url: 'https://randomuser.me/api/?results=12',
+  url: 'https://randomuser.me/api/?results=12&nat=gb,us,au',
   dataType: 'json',
   success: function(data) {
     employees = data.results;
@@ -13,13 +14,13 @@ $.ajax({
   }
 });
 
-
+// Function to capitalize the first letter of a string
 function capitalize(str) {
   let capitalizedStr = str.charAt(0).toUpperCase() + str.substr(1);
   return capitalizedStr;
 }
 
-
+// Function to capitalize the first letter of each word of a string
 function titleCase(str) {
    var splitStr = str.toLowerCase().split(' ');
    for (let i = 0; i < splitStr.length; i++) {
@@ -28,7 +29,7 @@ function titleCase(str) {
    return splitStr.join(' ');
 }
 
-
+// Function to format the DOB provided through API, into 00/00/00 format
 function formatDob(str) {
   let month = str.charAt(5) + str.charAt(6);
   let date = str.charAt(8) + str.charAt(9);
@@ -37,7 +38,7 @@ function formatDob(str) {
   return formatedDob;
 }
 
-
+// Function below appnds a modal of individual profile clicked on to the body of the page
 let arrayNum;
 function addModal() {
     $cards = $('.card');
@@ -64,12 +65,14 @@ function addModal() {
           </div>
         </div>`
       );
+      // Functions to add event listeners to elements of the modal
       addCloseModalEvent();
       addNextButtonEvent();
       addPrevButtonEvent();
   });
 }
 
+// Modal closes when close button is clicked
 function addCloseModalEvent() {
   let $modalContainer = $('.modal-container');
   let $modalCloseButton = $('#modal-close-btn');
@@ -78,11 +81,11 @@ function addCloseModalEvent() {
   });
 }
 
-
+// When 'next' button is clicked, the next profile appears in modal window
 function addNextButtonEvent() {
   let $nextButton = $('#modal-next');
   $nextButton.click(function() {
-    console.log('you did click it');
+    // to keep track of which profile is displayed
     arrayNum += 1;
     let $modalContainer = $('.modal-container');
     if (arrayNum < 12) {
@@ -112,16 +115,17 @@ function addNextButtonEvent() {
           addNextButtonEvent();
           addPrevButtonEvent();
       } else if (arrayNum >= 12){
+        // to prevent arrayNum from going above 11, thus exceeding the amount of profiles in Gallery
         arrayNum -= 1;
       }
     });
   }
 
-
+  // To add event listener to 'prev' button in the modal
   function addPrevButtonEvent() {
     let $prevButton = $('#modal-prev');
     $prevButton.click(function() {
-      console.log('you did click it');
+      // Decreases arrayNum to display previous profile
       arrayNum -= 1;
       let $modalContainer = $('.modal-container');
       if (arrayNum >= 0) {
@@ -151,11 +155,14 @@ function addNextButtonEvent() {
             addNextButtonEvent();
             addPrevButtonEvent();
           } else if (arrayNum < 0) {
+            // to prevent arrayNum from going below 0 and trying to display a profile that doesn't exist
             arrayNum += 1;
           }
       });
     }
 
+// Loops through the info retrieved from ajax call and displays the info in the
+// within the html specified below
 let arrayOfCards = [];
 function appendCardsToPage () {
   $.each(employees, function(i, person) {
@@ -170,5 +177,27 @@ function appendCardsToPage () {
         </div>
     </div>`);
   });
+  // Pushes each employee into the 'arrayOfCards' array, to be used in modal displays
   arrayOfCards.push(employees);
 }
+
+// Appends a search input to the page
+const $searchContainer = $('.search-container');
+$searchContainer.append(`<form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+</form>`);
+const $searchInput = $('#search-input');
+// Adds a keyup event to the search input, to filter cards by the name of the employee
+$searchInput.on('keyup', () => {
+  let search = $searchInput.val().toLowerCase();
+  let cards = document.querySelectorAll('.card');
+  let names = document.querySelectorAll('.card-name');
+  for (let i = 0; i < $cards.length; i++) {
+    if (names[i].innerHTML.indexOf(search) != -1) {
+      cards[i].style.display = '';
+    } else {
+      cards[i].style.display = 'none';
+    }
+  }
+});
